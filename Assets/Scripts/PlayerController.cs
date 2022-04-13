564dd04a51492848bec59e8257c9b4c465cbcfb6
João Pires <61878156+joaopires111@users.noreply.github.com>
2022-04-13 20:45:19 +0100

    clica M para aceder

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,78 +1,123 @@
 using UnityEngine;
+using System.Collections;
 // Include the namespace required to use Unity UI and Input System
 using UnityEngine.InputSystem;
-using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
 
-	// Create public variables for player speed, and for the Text UI game objects
-	public float speed;
-	public GameObject pianoPanel;
+    // Create public variables for player speed, and for the Text UI game objects
+    public float speed;
+    public GameObject pianoPanel;
+    public GameObject clicah;
+    public GameObject Nota335;
 
-	private float movementX;
-	private float movementY;
+    private float movementX;
+    private float movementZ;
 
-	private Rigidbody rb;
-	private int count;
+    private Rigidbody rb;
+    private int acertadas;
+    private bool entrouarea;
 
-	// At the start of the game..
-	void Start()
-	{
-		// Assign the Rigidbody component to our private rb variable
-		rb = GetComponent<Rigidbody>();
+    ArrayList Resultado = new ArrayList();
 
-		// Mete painel do piano a falso quando começa
-		pianoPanel.SetActive(false);
-	}
+    // At the start of the game..
+    void Start()
+    {
+        acertadas = 0;
+        Resultado.Add(3);
+        Resultado.Add(3);
+        Resultado.Add(5);
 
-	void FixedUpdate()
-	{
-		// Create a Vector3 variable, and assign X and Z to feature the horizontal and vertical float variables above
-		Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        entrouarea = false;
+        // Assign the Rigidbody component to our private rb variable
+        rb = GetComponent<Rigidbody>();
 
-		//exemplo do professor
-		//rb.AddForce(movement * speed);
+        // Mete painel do piano a falso quando começa
+        pianoPanel.SetActive(false);
+        clicah.SetActive(false);
+        Nota335.SetActive(false);
+    }
 
-		//velocidade sem acelaração (estilo rpg)
-		rb.velocity = movement * speed;
-
-	}
-
-	void OnTriggerEnter(Collider other)
-	{
-        //Abre painel do piano
-        if (other.gameObject.CompareTag("piano"))
-		{
+    void OnGUI()
+    {
+        if (Input.GetKeyDown("e") && entrouarea == true)
+        {
             pianoPanel.SetActive(true);
-		}
-	}
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Create a Vector3 variable, and assign X and Z to feature the horizontal and vertical float variables above
+        Vector3 movement = new Vector3(movementX, 0.0f, movementZ);
+
+        //exemplo do professor
+        //rb.AddForce(movement * speed);
+
+        //velocidade sem acelaração (estilo rpg)
+        rb.velocity = movement * speed;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("piano"))
+        {
+            //Abre painel do piano
+            clicah.SetActive(true);
+            entrouarea = true;
+
+        }
+    }
     private void OnTriggerExit(Collider other)
     {
-		//Fecha painel do piano
-		if (other.gameObject.CompareTag("piano"))
-		{
-			pianoPanel.SetActive(false);
-		}
-	}
+        //Fecha painel do piano
+        if (other.gameObject.CompareTag("piano"))
+        {
+            pianoPanel.SetActive(false);
+            clicah.SetActive(false);
+        }
+    }
 
     void OnMove(InputValue value)
-	{
-		Vector2 v = value.Get<Vector2>();
+    {
+        Vector2 v = value.Get<Vector2>();
 
-		movementX = v.x;
-		movementY = v.y;
+        movementX = v.x;
+        movementZ = v.y;
 
-		//roda o jogador esquerda e direita (scale negativa dá flip)
-		if(movementX > 0.0f)
+        //roda o jogador esquerda e direita (scale negativa dá flip)
+        if (movementX > 0.0f)
         {
-			transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
-		} 
-		else if (movementX < 0.0f)
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
+        else if (movementX < 0.0f)
         {
-			transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
-		}
+            transform.localScale = new Vector3(-0.5f, 0.5f, 0.5f);
+        }
 
-	}
+    }
+
+    public void clicatecla(int numerotecla)
+    {
+        if (acertadas != 3)
+        {
+            if (numerotecla == (int)Resultado[acertadas])
+            {
+                acertadas++;
+                Debug.Log(acertadas);
+                if (acertadas == 3)
+                {
+                    Nota335.SetActive(true);
+                }
+            }
+            else
+            {
+                acertadas = 0;
+            }
+        }
+
+    }
 
 }
